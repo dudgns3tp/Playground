64fc73191f6d19efe60c52bfee145044e10138bf
최영훈 <c_yh0327@naver.com>
2021-12-07 20:30:13 +0900

    cheerio

--- Rxjs/example/puppeteer.ts
+++ Rxjs/example/puppeteer.ts
@@ -1,18 +1,54 @@
 import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
+import { from } from 'rxjs';
 
-const GOOGLE = 'https://www.google.com';
+const url =
+    'https://select.ridibooks.com/search?q=%EB%8B%AC%EB%9F%AC%EA%B5%AC%ED%8A%B8&type=Books';
+const [TITLE, REDIRECT_URL] = [0, 1];
+const select = '#app > main > ul> li';
+const child = ['div > div > a > h3', 'div > div > a', 'a h3'];
+// (async () => {
+//     const browser: puppeteer.Browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(url, { waitUntil: 'networkidle0' });
+//     const content = await page.content();
+//     const $ = cheerio.load(content);
+//     $(select).each((index, list) => {
+//         const name = $(list).find(child[TITLE]).text();
+//         console.log(index, name);
+//     });
+//     await browser.close();
+// })();
+
 (async () => {
-    const browser: puppeteer.Browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(GOOGLE);
-    await page.screenshot({ path: 'screenshot.png' });
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const content = await page.content();
+    const $ = cheerio.load(content);
+    $(select).each((index, list) => {
+        const name = $(list).find(child[TITLE]).text();
+        console.log(index, name);
+    });
     await browser.close();
 })();
 
-(async () => {
-    const browser: puppeteer.Browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(GOOGLE);
-    await page.screenshot({ path: 'screenshot2.png' });
-    await browser.close();
-})();
+// return rxjs
+//     .from(urls)
+//     .pipe(
+//         orderedMergeMap(async (url) => {
+//             return withPage(browser)(async (page) => {
+//                 console.log(`Scraping ${url}`);
+//                 await page.goto(`${host}/${url}`);
+
+//                 const result = await page.evaluate(
+//                     (e) => e.textContent,
+//                     await page.$('#result')
+//                 );
+//                 console.log(`Scraping ${url} finished`);
+//                 return result;
+//             });
+//         }, 3),
+//         toArray()
+//     )
+//     .toPromise();
