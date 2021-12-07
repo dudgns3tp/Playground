058955cd81487dfd70441d5d10d3924a023333d3
최영훈 <c_yh0327@naver.com>
2021-12-07 20:43:53 +0900

    base code init

---
+++ OOP/class/server.ts
@@ -0,0 +1,18 @@
+import { DataStore } from './';
+export class Server {
+    store: DataStore = {
+        timestamp: 0,
+        data: '',
+    };
+
+    synchronize(clientDataStore: DataStore): DataStore {
+        if (clientDataStore.timestamp < this.store.timestamp) {
+            this.store = clientDataStore;
+            return undefined;
+        } else if (clientDataStore.timestamp < this.store.timestamp) {
+            return this.store;
+        } else {
+            return undefined;
+        }
+    }
+}
