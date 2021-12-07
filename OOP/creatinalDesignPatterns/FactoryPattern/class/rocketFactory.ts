dcaded933d1bc044e3ce85bea38edbd3e1adb614
최영훈 <c_yh0327@naver.com>
2021-12-07 22:08:33 +0900

    인스턴스 생성 분리

--- OOP/creatinalDesignPatterns/FactoryPattern/class/rocketFactory.ts
+++ OOP/creatinalDesignPatterns/FactoryPattern/class/rocketFactory.ts
@@ -3,16 +3,21 @@ import { Rocket } from './rocket';
 
 export class RocketFactory {
     buildRocket(): Rocket {
-        let rocket = new Rocket();
+        let rocket = this.createRocket();
         let payload = this.createPayload();
         let stages = this.createStages();
         rocket.payload = payload;
         rocket.stages = stages;
         return rocket;
     }
+    createRocket(): Rocket {
+        return new Rocket();
+    }
+
     createPayload(): Payload {
         return new Payload(0);
     }
+
     createStages(): Stage[] {
         let engine = new Engine(1000);
         let stage = new Stage([engine]);
